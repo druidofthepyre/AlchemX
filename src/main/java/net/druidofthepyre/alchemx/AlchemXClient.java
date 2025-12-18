4a60f60de536f21cd3b426b6246e6975ee385fd7
druidofthepyre <aidandalzell833@gmail.com>
2025-12-18 14:04:45 -0800

    first commit

---
+++ src/main/java/net/druidofthepyre/alchemx/AlchemXClient.java
@@ -0,0 +1,10 @@
+package net.druidofthepyre.alchemx;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AlchemXClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
