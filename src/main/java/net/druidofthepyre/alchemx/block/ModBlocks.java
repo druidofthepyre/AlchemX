0e1d915b2caabfd18371eebc3563e9ed12cd540a
druidofthepyre <aidandalzell833@gmail.com>
2025-12-18 21:40:09 -0800

    Assigned tags, added loot tables, added Abyssite Ore

--- src/main/java/net/druidofthepyre/alchemx/block/ModBlocks.java
+++ src/main/java/net/druidofthepyre/alchemx/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.druidofthepyre.alchemx.AlchemX;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -11,11 +12,19 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
+
+import javax.swing.*;
 
 public class ModBlocks {
     public static final Block ABYSSITE_BLOCK = registerBlock("abyssite_block",
             new Block(AbstractBlock.Settings.create()
-                    .strength(4f).requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+                    .strength(6f).requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+    private static final Block ABYSSITE_ORE = registerBlock("abyssite_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(5,8), AbstractBlock.Settings.create()
+                    .strength(5f).requiresTool().sounds(BlockSoundGroup.STONE)));
+
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItems(name, block);
@@ -32,6 +41,9 @@ public class ModBlocks {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(ABYSSITE_BLOCK);
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(ABYSSITE_ORE);
         });
     }
 }
